50dad7a72d7ab13105986d872aeddee4febfd249
amulyamari <amulya@saimail.com>
2018-07-27 15:31:58 +0000

    automatically backed up by learn

--- class_practice.rb
+++ class_practice.rb
@@ -2,4 +2,20 @@ class Facebookprofile
   def initialize (name, age)
   @name = name
   @age = age
+    puts "my name is #{name} and I am #{age} years old"
+  end
 end
+
+def bio = (bio)
+  @bio = bio
+end
+
+def bio
+  @bio
+end
+
+attr_accessor :job
+
+end
+becca= Facebookprofile.new("Becca", "17")
+becca.bio= "This is my bio"
